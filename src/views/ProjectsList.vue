5cfda898eb8deb64df7b4819ffddac9411d12335
dongkechang <dongkechang@nuctech.com>
2020-08-13 11:28:11 +0800

    删除不用的控件,留下plaintable

--- src/views/ProjectsList.vue
+++ src/views/ProjectsList.vue
@@ -15,13 +15,9 @@
 
 <script>
 import API from '@/api'
-import EChart from '@/components/chart/echart'
-import ProfileCard from '@/components/widgets/card/ProfileCard'
-import PlainTable from '@/components/widgets/list/PlainTable'
+import PlainTable from '@/components/widgets/list/PlainTable2'
 import PlainTableOrder from '@/components/widgets/list/PlainTableOrder'
 import Material from 'vuetify/es5/util/colors'
-import CircleStatistic from '@/components/widgets/statistic/CircleStatistic'
-import LinearStatistic from '@/components/widgets/statistic/LinearStatistic'
 export default {
   name: 'PageDashboard',
   components: {
@@ -30,95 +26,6 @@ export default {
   },
   data: () => ({
     color: Material,
-    selectedTab: 'tab-1',
-    linearTrending: [
-      {
-        subheading: 'Sales',
-        headline: '2,55',
-        caption: 'increase',
-        percent: 15,
-        icon: {
-          label: 'trending_up',
-          color: 'success'
-        },
-        linear: {
-          value: 15,
-          color: 'success'
-        }
-      },
-      {
-        subheading: 'Revenue',
-        headline: '6,553',
-        caption: 'increase',
-        percent: 10,
-        icon: {
-          label: 'trending_down',
-          color: 'error'
-        },
-        linear: {
-          value: 15,
-          color: 'error'
-        }
-      },
-      {
-        subheading: 'Orders',
-        headline: '5,00',
-        caption: 'increase',
-        percent: 50,
-        icon: {
-          label: 'arrow_upward',
-          color: 'info'
-        },
-        linear: {
-          value: 50,
-          color: 'info'
-        }
-      }
-    ],
-    trending: [
-      {
-        subheading: 'Email',
-        headline: '15+',
-        caption: 'email opens',
-        percent: 15,
-        icon: {
-          label: 'email',
-          color: 'info'
-        },
-        linear: {
-          value: 15,
-          color: 'info'
-        }
-      },
-      {
-        subheading: 'Tasks',
-        headline: '90%',
-        caption: 'tasks completed.',
-        percent: 90,
-        icon: {
-          label: 'list',
-          color: 'primary'
-        },
-        linear: {
-          value: 90,
-          color: 'success'
-        }
-      },
-      {
-        subheading: 'Issues',
-        headline: '100%',
-        caption: 'issues fixed.',
-        percent: 100,
-        icon: {
-          label: 'bug_report',
-          color: 'primary'
-        },
-        linear: {
-          value: 100,
-          color: 'error'
-        }
-      }
-    ]
   }),
   computed: {
     activity() {
