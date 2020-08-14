e7d0ca1a4c894f1aabf48d66415b44028b5c9568
dongkechang <dongkechang@nuctech.com>
2020-08-14 10:56:08 +0800

    修改工程列表样式,添加编辑,删除图标

--- src/components/widgets/list/PlainTable2.vue
+++ src/components/widgets/list/PlainTable2.vue
@@ -20,32 +20,20 @@
             :color="item.color"
           />
         </template>
-        <template v-slot:item.action="{ item }">
-          <v-menu>
-            <template v-slot:activator="{ on: menu }">
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on: tooltip }">
-                  <v-btn icon v-on="{ ...menu }">
-                    <v-icon>mdi-dots-vertical</v-icon></v-btn
-                  >
-                </template>
-                <span>Action</span>
-              </v-tooltip>
-            </template>
-            <v-list class="pa-0" dense>
-              <v-list-item
-                v-for="action in actions"
-                :key="action.text"
-                @click="action.click(item)"
-              >
-                <v-list-item-icon class="mr-2">
-                  <v-icon small>{{ action.icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>{{ action.text }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+          >
+            mdi-delete
+          </v-icon>
         </template>
+
       </v-data-table>
       <v-divider />
     </v-card-text>
@@ -53,48 +41,47 @@
 </template>
 
 <script>
-import { Projects } from '@/api/project2'
-export default {
-  data() {
-    return {
-      headers: [
-        { text: '项目ID',
-          align: 'center',
-          sortable: true,
-          value: 'id'
-        },
-        {
-          text: '工程名称',
-          align: 'left',
-          value: 'name'
-        },
-        { text: '传感器地址范围', value: 'deadline' },
-        { text: '进度', value: 'progress' },
-        { text: '动作', value: 'action', align: 'right' }
-      ],
-      actions: [
-        {
-          text: 'View Item',
-          icon: 'mdi-eye',
-          click: this.handleViewItem
-        },
-        {
-          text: 'Edit Item',
-          icon: 'mdi-pencil',
-          click: this.handleEditItem
-        },
-        {
-          text: 'Delete Item',
-          icon: 'mdi-close',
-          click: this.handleDeleteItem
-        }
-      ]
-    }
-  },
-  computed: {
-    projects() {
-      return Projects
-    }
-  }
-}
+ import { Projects } from '@/api/project2'
+ export default {
+   data() {
+     return {
+       headers: [
+         { text: '项目ID',
+           sortable: true,
+           value: 'id'
+         },
+         {
+           text: '工程名称',
+           align: 'left',
+           value: 'name'
+         },
+         { text: '传感器地址范围', value: 'deadline' },
+         { text: '进度', value: 'progress' },
+         { text: '动作', value: 'actions', align: 'right' }
+       ],
+       actions: [
+         {
+           text: 'View Item',
+           icon: 'mdi-eye',
+           click: this.handleViewItem
+         },
+         {
+           text: 'Edit Item',
+           icon: 'mdi-pencil',
+           click: this.handleEditItem
+         },
+         {
+           text: 'Delete Item',
+           icon: 'mdi-close',
+           click: this.handleDeleteItem
+         }
+       ]
+     }
+   },
+   computed: {
+     projects() {
+       return Projects
+     }
+   }
+ }
 </script>
